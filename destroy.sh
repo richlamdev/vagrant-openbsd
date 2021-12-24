fe58faa9a960d8ae6637e9c66eda4da207af267f
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2021-12-23 23:09:07 -0800

    modifty destroy script to remove appropriate ssh host-keys

--- destroy.sh
+++ destroy.sh
@@ -2,6 +2,6 @@
 
 vagrant destroy -f
 
-ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R "japan.test"
+ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R "10.0.0.2"
 
-ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R "india.test"
+ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R "10.0.0.3"
