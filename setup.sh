6cfadb63cdd461cee306da63305b8c105e4800a8
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2021-12-27 01:44:52 -0800

    disable strict host key checking in setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R "192.168.150.202"
 # 2) This is not for production
 # 3) Normal users should be provisioned access via SSH key, after python3 is installed on OpenBSD
 # 4) The root user password should be much longer and complex!
-ssh vagrant@192.168.150.201 -i ~/.vagrant.d/insecure_private_key 'echo -e "password123\npassword123" | sudo passwd root'
-ssh vagrant@192.168.150.202 -i ~/.vagrant.d/insecure_private_key 'echo -e "password123\npassword123" | sudo passwd root'
+ssh vagrant@192.168.150.201 -i ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" 'echo -e "password123\npassword123" | sudo passwd root'
+ssh vagrant@192.168.150.202 -i ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" 'echo -e "password123\npassword123" | sudo passwd root'
 
 exit 0
