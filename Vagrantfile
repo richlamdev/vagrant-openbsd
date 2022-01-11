0aab40b5bcad14a06b48379a9d4db3a615ec3421
Richard Lam <54393902+richlamdev@users.noreply.github.com>
2022-01-11 01:01:50 -0800

    add ubuntu client system

--- Vagrantfile
+++ Vagrantfile
@@ -15,13 +15,31 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |openbsdconfig|
   openbsdconfig.vm.define "japan" do |japan|
     japan.vm.box = "generic/openbsd7"
     japan.vm.hostname = "japan.test"
-    japan.vm.network :public_network, bridge: "enp9s0", ip: "192.168.150.201"
+    japan.vm.network :public_network, bridge: "enp9s0", ip: "192.168.13.201"
   end
 
-  openbsdconfig.vm.define "india" do |india|
-    india.vm.box = "generic/openbsd7"
-    india.vm.hostname = "india.test"
-    india.vm.network :public_network, bridge: "enp9s0", ip: "192.168.150.202"
+  #openbsdconfig.vm.define "india" do |india|
+    #india.vm.box = "generic/openbsd7"
+    #india.vm.hostname = "india.test"
+    #india.vm.network :public_network, bridge: "enp9s0", ip: "192.168.13.202"
+  #end
+
+end
+
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |ubuntuconfig|
+  ubuntuconfig.ssh.insert_key = false
+  ubuntuconfig.vm.synced_folder ".", "/vagrant", disabled: true
+  ubuntuconfig.vm.provider :virtualbox do |v|
+    v.memory = 512
+    v.linked_clone = true
+  end
+
+  # client
+  ubuntuconfig.vm.define "client" do |client|
+    client.vm.box = "geerlingguy/ubuntu2004"
+    client.vm.hostname = "client.test"
+    client.vm.network :public_network, bridge: "enp9s0", ip: "192.168.13.210"
   end
 
 end
--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # remove previous host keys, if any
-ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R "192.168.150.201"
-ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R "192.168.150.202"
+ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R "192.168.13.201"
+#ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R "192.168.13.202"
 
 # set root password to: password123.
 # this is VERY insecure, obviously.  For testing purposes only!!
@@ -12,7 +12,8 @@ ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R "192.168.150.202"
 # 2) This is not for production
 # 3) Normal users should be provisioned access via SSH key, after python3 is installed on OpenBSD
 # 4) The root user password should be much longer and complex!
-ssh vagrant@192.168.150.201 -i ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" 'echo -e "password123\npassword123" | sudo passwd root'
-ssh vagrant@192.168.150.202 -i ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" 'echo -e "password123\npassword123" | sudo passwd root'
+ssh vagrant@192.168.13.201 -i ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" 'echo -e "password1\npassword1" | sudo passwd root'
+ssh vagrant@192.168.13.202 -i ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" 'echo -e "password1\npassword1" | sudo passwd root'
+ssh vagrant@192.168.13.210 -i ~/.vagrant.d/insecure_private_key -o "StrictHostKeyChecking no" 'echo -e "password1\npassword1" | sudo passwd root'
 
 exit 0
